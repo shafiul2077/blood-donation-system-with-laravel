7c2ded5f982c66792256edfb647180933f570d69
Shafiul Bashar <shafiul2077@gmail.com>
2023-01-16 01:28:57 +0600

    addAcceptors function done

--- app/Http/Controllers/acceptorsControoler.php
+++ app/Http/Controllers/acceptorsControoler.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\acceptor;
+use  Session;
 
 class acceptorsControoler extends Controller
 {
@@ -13,6 +15,15 @@ class acceptorsControoler extends Controller
 
     public function addAcceptors(Request $request){
 
+        $acceptors= new acceptor();
+        $acceptors->name=$acceptors->name;
+        $acceptors->blood_group=$acceptors->blood_group;
+        $acceptors->age=$acceptors->age;
+        $acceptors->address=$acceptors->address;
+        $acceptors->contact_number=$acceptors->contact_number;
+        $acceptors-> save();
+        Session:: flash('msg','Sent Successfully!');
+        return redirect('contact');
     }
 
 }
